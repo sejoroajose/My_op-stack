27bfe917b47b463e84d84747d3fb3c76b56c60ea
sejoroajose <ajosesejoro@gmail.com>
2024-08-06 13:20:22 +0100

    creating  the dao contract for the crowd funding

---
+++ packages/contracts-bedrock/src/governance/DAO.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "./GovernanceToken.sol";
+
+// Mock World ID verification interface
+interface IWorldID {
+    function isVerified(address _user) external view returns (bool);
+}
+
+/// @title DAO
+/// @notice A DAO contract for a crowdfunding platform using governance tokens and World ID verification.
+contract DAO is Ownable {
+}
+
