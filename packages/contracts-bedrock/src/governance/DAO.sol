f33b97213f0e35e63545aa75eaad20a48f3349b6
sejoroajose <ajosesejoro@gmail.com>
2024-08-06 15:12:43 +0100

    defined the required modifiers

--- packages/contracts-bedrock/src/governance/DAO.sol
+++ packages/contracts-bedrock/src/governance/DAO.sol
@@ -45,5 +45,37 @@ contract DAO is Ownable {
     event Voted(uint256 indexed campaignId, address voter, bool support);
     event CampaignFinalized(uint256 indexed campaignId, bool success);
     event Withdrawn(uint256 indexed campaignId, address beneficiary, uint256 amount);
+
+    // Modifiers
+    modifier onlyVerified() {
+        required(worldID.isVerified(msg.sender), "BoostDAO: User is not verified");
+        _;
+    }
+
+    modifier campaignExists(uint256 campaignId) {
+        require(campaignId < campaigns.length, "BoostDAO: Campaign does not exist")
+        _;
+    }
+
+    modifier campaignOpen(uint256 campaignId) {
+        require(block.timestamp < campaigns[campaignId].deadline, "BoostDAO: Campaign has ended");
+        _;
+    }
+
+    modifier campaignClosed(uint256 campaignId) {
+        require(block.timestamp >= campaigns[campaignId], "BoostDAO: Campaign is still open");
+        _;
+    }
+
+    modifier hasContributed(uint256 campaignId) {
+        require(campaigns[campaignId].contributions[msg.sender] > 0, "BoostDAO: No contribution made");
+        _;
+    }
+
+    modifier hasNotVoted(uint256 campaignId) {
+        require(!campaigns[campaignId].hasVoted[msg.sender], "BoostDAO: Voter has already voted");
+        _;
+    }
+
 }
 
