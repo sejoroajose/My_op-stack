47ce05a90712cc0137816457c7135fcfe365bb40
sejoroajose <ajosesejoro@gmail.com>
2024-08-08 09:18:15 +0100

    converting to nextjs app and creating the home page

---
+++ boostdao/pages/page.tsx
@@ -0,0 +1,10 @@
+import CampaignList from '../components/CampaignList';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>BoostDAO</h1>
+      <CampaignList />
+    </div>
+  );
+}
