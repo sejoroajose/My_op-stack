14c6acfdf3df0b835f48bbef2a5e4943b875c221
sejoroajose <ajosesejoro@gmail.com>
2024-08-08 09:22:34 +0100

    create campaign page

---
+++ boostdao/pages/create-campaign/page.tsx
@@ -0,0 +1,10 @@
+import CreateCampaign from '../components/CreateCampaign';
+
+export default function CreateCampaignPage() {
+  return (
+    <div>
+      <h1>Create Campaign</h1>
+      <CreateCampaign />
+    </div>
+  );
+}
