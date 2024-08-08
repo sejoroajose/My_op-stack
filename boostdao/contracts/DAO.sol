a0fa06e52f4dc07c3322983250776a509bce39ec
sejoroajose <ajosesejoro@gmail.com>
2024-08-08 09:11:05 +0100

    updates

--- boostdao/contracts/DAO.sol
+++ boostdao/contracts/DAO.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.15;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "../packages/contracts-bedrock/src/governance/GovernanceToken.sol";
+import "./GovernanceToken.sol";
 
 // Mock World ID verification interface
 interface IWorldID {
